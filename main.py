bc7961267c5ebdd4aac68eaaca0d068204590d03
martina.gauci@aqubix.com <martina.gauci@aqubix.com>
2022-03-30 17:03:10 +0100

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+# initial tutorial https://stackabuse.com/image-recognition-in-python-with-tensorflow-and-keras/
+
 import numpy
 from tensorflow import keras
 from keras.constraints import maxnorm
 from keras.utils import np_utils
+from keras.datasets import cifar10
 
-print("hello")
+# Set random seed for purposes of reproducibility
+seed = 21
+
+# Loading in the data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+# Normalize the inputs from 0-255 to between 0 and 1 by dividing by 255
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# One-hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+class_num = y_test.shape[1]
+
